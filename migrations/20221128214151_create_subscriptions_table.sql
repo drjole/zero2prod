e0399dd01e01afecf8f970f97379ceca3c678839
Jonas Weber <jonas@jole.dev>
2022-11-29 22:15:31 +0100

    Continue working

--- migrations/20221128214151_create_subscriptions_table.sql
+++ migrations/20221128214151_create_subscriptions_table.sql
@@ -1,9 +1,9 @@
 -- Add migration script here
 CREATE TABLE subscriptions
 (
-    id             uuid        NOT NULL,
+    id            uuid        NOT NULL,
     PRIMARY KEY (id),
-    email          TEXT        NOT NULL UNIQUE,
-    name           TEXT        NOT NULL,
-    subscribed_att timestamptz NOT NULL
+    email         TEXT        NOT NULL UNIQUE,
+    name          TEXT        NOT NULL,
+    subscribed_at timestamptz NOT NULL
 );
